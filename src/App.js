3febe07c8a42610cdb590668e8c6002e67a42e46
karamalqinneh <karamalqinneh@live.com>
2022-04-26 01:09:00 +0300

    solved the results issue

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import Header from "./components/header/header";
 import Form from "./components/form/form";
 import History from "./components/histroy/history";
-// import Results from "./components/results/results";
+import Results from "./components/results/results";
 import { useState } from "react";
 
 function App() {
-  // let [resultsData, setRestultsData] = useState("");
+  let [resultsData, setRestultsData] = useState("");
   let localData = JSON.parse(localStorage.getItem("RESTyHistory"));
   let [renderedData, setRenderedData] = useState(localData);
   const getFormDataHandler = (data) => {
-    // localData ? (localData = localData) : (localData = []);
+    localData ? (localData = localData) : (localData = []);
     localStorage.setItem("RESTyHistory", JSON.stringify([...localData, data]));
-    // setRestultsData(data);
+    setRestultsData(data);
     setRenderedData(JSON.parse(localStorage.getItem("RESTyHistory")));
   };
   return (
     <>
       <Header></Header>
       <Form getFormData={getFormDataHandler}></Form>
-      <History
-        getHistoryData={renderedData == null ? [] : renderedData}
-      ></History>
-      {/* <Results getResultsData={resultsData}></Results> */}
+      <div style={{ display: "flex" }}>
+        <History
+          getHistoryData={renderedData == null ? [] : renderedData}
+        ></History>
+        <Results getResultsData={resultsData}></Results>
+      </div>
     </>
   );
 }
